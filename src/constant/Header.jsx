8eb5a724df4392f6eae38bc512de048cd5c5e18c
shubhhhhhhh <heavyprogrammer7@gmail.com>
2022-09-27 23:37:26 +0530

    fb button

--- src/constant/Header.jsx
+++ src/constant/Header.jsx
@@ -9,12 +9,12 @@ export default function Header() {
                 <Container fluid style={{ backgroundColor: "#f6f6f6", padding: "0.8rem 1.5rem" }}>
                     <Row md={12} xs={12}>
                         <Col md={3}>
-                            <div  style={{ height:"4.5rem" }}>
+                            <div  style={{ height:"4.5rem", width:"30vw" }}>
                                 <div style={{display: "inline-block",padding:"0.1rem 0rem 0rem 0.6rem" }}>
                                     <img src={logo} alt="" height={"70rem"} />
                                 </div>
                                 <div style={{display:"inline-block"}}>
-                                <div style={{ display: "inline-block",position:"absolute",margin:"-1.7rem 0rem 0rem 1rem" }}>
+                                <div style={{width:"20vw", display: "inline-block",position:"absolute",margin:"-1.7rem 0rem 0rem 1rem" }}>
                                     <span style={{ fontSize: "1.9rem", fontWeight: "bold",display:"block" }}>PointCapitalis</span>
                                     <span style={{ fontSize: "1rem" ,display:"block"}}>Wealth Builders Point</span>
                                 </div>    
